2e4b54996e5f8e2f98fd9e8f987a3ed7f08db84a
Marko Mijac <mmijac@foi.hr>
2024-11-05 11:41:00 +0100

    Add configuration repository with default configuration.

---
+++ AIMPersistence/ConfigurationRepository.cs
@@ -0,0 +1,38 @@
+using System;
+using AIMCore;
+using AIMCore.Communication;
+using AIMCore.Configurations;
+using AIMCore.Sensors;
+using AIMSmartScale.Parsers;
+
+namespace AIMPersistence;
+
+public class ConfigurationRepository
+{
+    public Configuration GetDefaultConfiguration()
+    {
+        var comStrategyFactory = new DefaultCommunicationFactory();
+        var measurementParserFactory = new ScaleMeasurementParserFactory();
+
+        var uartStrategy = comStrategyFactory.GetCommunicationStrategies().OfType<UARTCommunication>().First();
+        var parser = measurementParserFactory.GetParsers().OfType<ScaleMeasurementParser>().First();
+
+        var instrument = new DefaultSensor("SCALE", "GET_WEIGHT", uartStrategy, parser);
+
+        var sensors = new List<ISensor>();
+        var gyroscope = new DefaultSensor("GYROSCOPE", "GET_TILT", uartStrategy, parser);
+        var vibrationsensor = new DefaultSensor("VIBRATIONSENSOR", "GET_VIBR", uartStrategy, parser);
+        sensors.Add(gyroscope);
+        sensors.Add(vibrationsensor);
+
+        var aiModel = new AIModel("ScaleModel", @"/home/ubuntustudio/Documents/AimFiles/Models/ScaleModel.onnx");
+
+        var builder = new ConfigurationBuilder();
+        builder.SetName("ScaleConfig")
+        .SetInstrument(instrument)
+        .SetSensors(sensors)
+        .SetAIModel(aiModel);
+
+        return builder.Build();
+    }
+}
